e3988252ac864757b2b802005edb1a6580437969
Ihor <132585512+IhorZolot@users.noreply.github.com>
2023-08-12 12:25:34 +0300

    ref

--- src/component/Counter/Counter.jsx
+++ src/component/Counter/Counter.jsx
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
   const [input, setInput] = useState(1);
   const [step, setStep] = useState(1);
+
+  useEffect(() => {
+    if (counter === 3) {
+      console.log("hello");
+    }
+    if (counter === 5) {
+      setStep(11);
+    }
+    if (counter >= 100) {
+      resetCounter();
+    }
+  }, [counter]);
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    console.log(inputRef);
+    console.log(inputRef.current);
+    inputRef.current.focus();
+  }, [step]);
 
   const plusFive = (msg) => {
     setCounter((prev) => prev + 5);
@@ -27,7 +47,12 @@ export const Counter = () => {
   return (
     <div>
       <h2>{counter}</h2>
-      <input type="text" onChange={handleChageInput} value={input} />
+      <input
+        type="text"
+        onChange={handleChageInput}
+        value={input}
+        ref={inputRef}
+      />
       <button onClick={setStepClick}>Set Step</button>
       <button onClick={() => setCounter((perv) => perv - step)}>
         Button -
